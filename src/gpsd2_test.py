eca7019f0c81c02027a7de74e0bfd743060f3e59
Michael Torp Kaalund <kaalund@gmail.com>
2024-09-16 21:33:15 +0200

    Moved alittle around

--- src/gpsd2_test.py
+++ src/gpsd2_test.py
@@ -3,14 +3,12 @@ import gpsd2
 # Connect to the local gpsd
 gpsd2.connect()
 
-# Get GPS position
-packet = gpsd2.get_current()
-
 device = gpsd2.device()
-
 print(device)
 
 try:
+    # Get GPS position
+    packet = gpsd2.get_current()
     print(packet.position())
-except NoFixError as err:
+except gpsd2.NoFixError as err:
     print("NoFixError:", err)
