ef6a6a17732d37c91b13c79dfedea179e83de0c1
Michael Torp Kaalund <kaalund@gmail.com>
2024-09-17 22:10:33 +0200

    just soom display test

--- src/lcd_display.py
+++ src/lcd_display.py
@@ -3,4 +3,4 @@ from RPLCD.i2c import CharLCD
 lcd = CharLCD(i2c_expander='PCF8574', address=0x27, port=1, cols=20, rows=4, dotsize=8)
 lcd.clear()
 
-lcd.write_string('Hello, World')
+lcd.write_string('Hello, World! My goal is a good one')
