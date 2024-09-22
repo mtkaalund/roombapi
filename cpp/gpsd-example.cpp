41d1e1c465e943903476e0a2ab85096d476a09c1
Michael Torp Kaalund <kaalund@gmail.com>
2024-09-22 20:37:45 +0200

    small updates to gpsd-example

--- cpp/gpsd-example.cpp
+++ cpp/gpsd-example.cpp
@@ -110,7 +110,7 @@ auto main() -> int {
     const auto time_str{TimespecToTimeStr(gpsd_data->fix.time, UTC)};  // you can change the 2nd argument to LOCALTIME, UTC, UNIX or ISO8601
 
     std::cout << std::setprecision(8) << std::fixed;  // set output to fixed floating point, 8 decimal precision
-    std::cout << time_str << "\n\tLatitude: " << latitude << " Longitude: " << longitude << "\n";
+    std::cout << time_str << "\n\tLatitude: " << latitude << " Longitude: " << longitude << " Speed: " << speed << "\n";
     std::cout << "\tHDOP: " << hdop << " " << get_dop_meaning(hdop) << " VDOP: " << vdop << " " << get_dop_meaning(vdop) << " PDOP: " << pdop << " " << get_dop_meaning(pdop) << "\n";
     std::cout << "\tSatellites visible: " << s_vis << " Satellites used: " << s_used << "\n";
     for(int i = 0; i < s_vis; i++)
