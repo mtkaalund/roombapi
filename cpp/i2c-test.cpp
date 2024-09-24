39762f62d3cd4efcd8df0b9f22b55b52f7b8b68c
Michael Torp Kaalund <kaalund@gmail.com>
2024-09-24 22:39:30 +0200

    using the class

--- cpp/i2c-test.cpp
+++ cpp/i2c-test.cpp
@@ -1,66 +1,66 @@
 #include <iostream>
-#include <string>
-#include <cerrno>
-#include <cstring>
-// Linux specific headers
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/ioctl.h>
-#include <linux/input.h>
-#include <linux/i2c-dev.h>
-#include <linux/i2c.h>
 
-// https://raspberry-projects.com/pi/programming-in-c/i2c/using-the-i2c-interface
+#include "i2c.h"
 
-auto main() -> int {
+auto main() -> int
+{
 
-    int file_i2c;
-    int length;
-    unsigned char buffer[60] = {0};
+    rpi::i2c i2c_device = rpi::i2c(1);
 
-    // Opening i2c bus
-    std::string filename = "/dev/i2c-1";
-    if( (file_i2c = open(filename.c_str(), O_RDWR)) < 0 )
-    {
-        // TODO: Check errno to get what went wrong
-        int err = errno;
-        std::cerr << "Failed to open the i2c bus: " << strerror(err) << std::endl;
-        return -1;
-    }
+    i2c_device.send(0x27, 0x01);
+    i2c_device.send(0x27, 0x02);
+    i2c_device.send(0x27, 0x04);
+    i2c_device.send(0x27, 0x12);
 
-    int i2c_addr = 0x27;
-    if( ioctl(file_i2c, I2C_SLAVE, i2c_addr) < 0 )
-    {
-        // TODO: Check errno to get what went wrong
-        close(file_i2c);
-        int err = errno;
-        std::cerr << "Failed to acquire bus access and/or talk to slave." << strerror(err) << std::endl;
-        return -1;
-    }
+    // int file_i2c;
+    // int length;
+    // unsigned char buffer[60] = {0};
 
-    // Read bytes
-    length = 4; // Number of bytes to read
-    if( read(file_i2c, buffer, length) != length ) {
-        // Read
-        close(file_i2c);
-        int err = errno;
-        std::cerr << "Failed to read from the i2c bus." << strerror(err) << std::endl;
-        return -1;
-    }
+    // // Opening i2c bus
+    // std::string filename = "/dev/i2c-1";
+    // if ((file_i2c = open(filename.c_str(), O_RDWR)) < 0)
+    // {
+    //     // TODO: Check errno to get what went wrong
+    //     int err = errno;
+    //     std::cerr << "Failed to open the i2c bus: " << strerror(err) << std::endl;
+    //     return -1;
+    // }
 
-    std::cout << "Data read: " << buffer << std::endl;
+    // int i2c_addr = 0x27;
+    // if (ioctl(file_i2c, I2C_SLAVE, i2c_addr) < 0)
+    // {
+    //     // TODO: Check errno to get what went wrong
+    //     close(file_i2c);
+    //     int err = errno;
+    //     std::cerr << "Failed to acquire bus access and/or talk to slave." << strerror(err) << std::endl;
+    //     return -1;
+    // }
 
-    buffer[0] = 0x01;
-    buffer[1] = 0x02;
-    length = 2;
-    if( write(file_i2c, buffer, length) != length ) {
-        close(file_i2c);
-        int err = errno;
-        std::cerr << "Failed to write to the i2c bus." << strerror(err) << std::endl;
-        return -1;
-    }
+    // // Read bytes
+    // length = 4; // Number of bytes to read
+    // if (read(file_i2c, buffer, length) != length)
+    // {
+    //     // Read
+    //     close(file_i2c);
+    //     int err = errno;
+    //     std::cerr << "Failed to read from the i2c bus." << strerror(err) << std::endl;
+    //     return -1;
+    // }
 
-    close(file_i2c);
+    // std::cout << "Data read: " << buffer << std::endl;
+
+    // buffer[0] = 0x01;
+    // buffer[1] = 0x02;
+    // length = 2;
+    // if (write(file_i2c, buffer, length) != length)
+    // {
+    //     close(file_i2c);
+    //     int err = errno;
+    //     std::cerr << "Failed to write to the i2c bus." << strerror(err) << std::endl;
+    //     return -1;
+    // }
+
+    // close(file_i2c);
 
     return 0;
 }
