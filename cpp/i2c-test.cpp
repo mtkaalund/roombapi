8d667efd87963f35e42dc11676fa2b1f773dc4f3
Michael Torp Kaalund <kaalund@gmail.com>
2024-09-27 23:34:05 +0200

    trying to make a lambda function for sending values to lcd

--- cpp/i2c-test.cpp
+++ cpp/i2c-test.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 
+#include <thread>
+#include <chrono>
+
 #include "i2c.h"
 
 namespace lcd {
@@ -47,7 +50,7 @@ enum class display_control
 #define RS (1 << 0)
 #define RW (1 << 1)
 #define CS (1 << 2)
-#define P3 (1 << 3)
+#define BL (1 << 3)     // Backlight
 #define DB4 (1 << 4)
 #define DB5 (1 << 5)
 #define DB6 (1 << 6)
@@ -58,14 +61,47 @@ auto main() -> int
 
     rpi::i2c i2c_device = rpi::i2c(1);
 
-    i2c_device.send(0x27, 0x00);
+    unsigned char c = 0x00;
 
-    for(int i = 0; i < 20000; i++)
-    for(int j = 0; j < 20000; j++)
-        asm("nop");
+    // This will be 4-bit operation mode
+    // Setup lcd display
+    //       RS   RW  DB7     DB6     DB5     DB4
+    //1:      0    0   0       0       1       0      // Sets to 4-bit operations
+    //2:      0    0   0       0       1       0      // Sets 4-bit operations and 1-line 5x8
+    //        0    0   0       0       *       *
+    //3:      0    0   0       0       0       0      // Display on. Cursor on
+    //        0    0   0       1       1       0
+    //4:      0    0   0       0       0       0      // Set mode to increment and shift right
+    //        0    0   0       1       1       0
+    //5:      1    0   0       1       0       0      // Writes H
+    //        1    0   1       0       0       0    
+    //
+    auto lcd_send = [](rpi::i2c device, uint8_t value, uint8_t mode) {
+        uint8_t high = value & 0xF0;
+        uint8_t low  = ( value << 4 ) & 0xF0;
 
+        const uint8_t backlight = 0x08;
+        const uint8_t enable    = 0b00000100;
 
-    i2c_device.send(0x27, P3);
+        device.send(0x27, high | mode | backlight);
+        // enable
+        device.send(0x27, (high | mode | backlight) | enable );
+        std::this_thread::sleep_for(std::chrono::microseconds(1));
+        device.send(0x27, (low | mode | backlight) & ~enable);
+        std::this_thread::sleep_for(std::chrono::microseconds(50));
+        // enable
+    };
+
+    const uint8_t mode = 0x00;
+
+    lcd_send(i2c_device, (0x03 << 4), mode);
+    std::this_thread::sleep_for(std::chrono::microseconds(4500));
+    lcd_send(i2c_device, (0x03 << 4), mode);
+    std::this_thread::sleep_for(std::chrono::microseconds(4500));
+    lcd_send(i2c_device, (0x03 << 4), mode);
+    std::this_thread::sleep_for(std::chrono::microseconds(150));
+    lcd_send(i2c_device, (0x02 << 4), mode);
+    
 
     return 0;
 }
